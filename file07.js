56525d0fcdf8f7bf81041b5778f417d7ff88d80b
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-20 20:41:08 +0530

    modified

--- file07.js
+++ file07.js
@@ -1,13 +1,22 @@
-//trim()
-//toUpperCase()
-//toLowerCase()
-//slice
+//trim()-->removes the empty spaces
+//toUpperCase()-->makes strings in capital format
+//toLowerCase()-->makes string in lower case format
+//slice-->used to extract a portion of an array or string.
  
 // let firstName = "  abhishek  ";
-let firstName = "abhishek";
+let firstName = "AbhiShek";
 // console.log(firstName);
 console.log(firstName.length);
 
-// firstName = firstName.trim(); //"abhishek" -->removes the empty spaces
+// firstName = firstName.trim(); //"abhishek" 
 // console.log(firstName);
 // console.log(firstName.length);
+
+// firstName = firstName.toUpperCase();
+firstName = firstName.toLowerCase();
+console.log(firstName);
+
+// let newString = firstName.slice(0,4);//abhi
+// let newString = firstName.slice(0,6);//abhish
+let newString = firstName.slice(1);//bhishek
+console.log(newString);
