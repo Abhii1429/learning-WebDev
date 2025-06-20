60a678c663709becd08d3bd698cb2c8aea6931fd
Abhishek Pandey <121479460+Abhii1429@users.noreply.github.com>
2025-06-20 12:47:51 +0000

    Intro to var

---
+++ JS/file02.js
@@ -0,0 +1,13 @@
+"use strict";
+//Intro to Variable
+
+//declare a variable
+var firstName = "Abhishek";
+
+//use a variable
+console.log(firstName);
+
+// change value
+firstName = "Sid";
+
+console.log(firstName);
