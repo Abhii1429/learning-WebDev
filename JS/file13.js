6446403082e65904f6994427647a8bc0974cf424
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-21 20:26:31 +0530

    truthy & falsy values

---
+++ JS/file13.js
@@ -0,0 +1,24 @@
+// truthy & falsy value
+
+//falsy values:
+// false
+// " "
+//null
+// undefined
+// 0
+
+// truthy value
+//"abc" ; 1; -1
+
+
+// let firstName = "";
+// let firstName;
+// let firstName = "";
+// let firstName = false;
+let firstName = "abhishek";
+
+ if(firstName){
+     console.log(firstName);
+ }else{
+    console.log("firstName is kinda empty");
+ }
