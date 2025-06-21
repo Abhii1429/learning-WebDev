0b85fbeab1bf634e1b59128ec961d3c761aa9330
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-21 20:27:03 +0530

    if-else

---
+++ JS/file14.js
@@ -0,0 +1,16 @@
+// if else condition
+
+let age = 18; 
+ if(age >= 18){
+    console.log("User can play minecraft");
+ }else{
+    console.log("User can play Temple Run");
+ }
+
+
+ let num = 13;
+ if(num%2 === 0){
+    console.log("even");
+ }else{
+    console.log("odd");
+ }
