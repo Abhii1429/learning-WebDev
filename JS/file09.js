c52042518467ed56d7f8d969aa2fff292b19f3f7
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-21 17:43:43 +0530

    String concatenation

---
+++ JS/file09.js
@@ -0,0 +1,20 @@
+//string concatenation
+
+// let string1 = "abhishek";
+// let string2 = "pandey";
+
+// let fullName = string1 + " " + string2; //concatenation
+// console.log(fullName);
+
+// let string1 = "17";
+// let string2 = "29";
+
+// let newStr = string1 + string2;
+// console.log(newStr);//1729
+
+let string1 = "17";
+let string2 = "29";
+
+let newStr = +string1 + +string2;
+// console.log(newStr);//17+29=46
+console.log(typeof newStr);
