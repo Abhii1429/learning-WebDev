9858e8dc7ac8db410dc450fc31941074fad5809d
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-21 14:10:56 +0530

    typeof operator

---
+++ JS/file08.js
@@ -0,0 +1,22 @@
+// "typesof" operator
+
+// let age = 22;
+// let firstName = "abhishek";
+ 
+// console.log(typeof age);
+
+//convert number to string
+
+// console.log( typeof (age + "")); //string
+//parenthesis after typeof is not mandatory
+
+//convert string to number
+
+//  let myStr = +"34";
+//  console.log(typeof myStr); //number
+
+let age  = "18";
+
+// age = String(age);
+age = Number(age);
+console.log(typeof age);
