eb9500b12a82ab60b7e8e6997361b24a40c70c9c
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-22 19:59:12 +0530

    for loop

---
+++ JS/file22.js
@@ -0,0 +1,13 @@
+// intro to for loop
+
+
+for(let i = 0; i <= 9; i++){
+     console.log(i)
+}
+console.log(i);// let exist only inside loop cannot be accessed outside the loop
+
+
+for(var i = 0; i <= 9; i++){
+     console.log(i)
+}
+console.log(i);// var can be accessed from outside the loop also
