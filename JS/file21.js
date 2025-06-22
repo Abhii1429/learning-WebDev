75e623178a29181ea9533a36aaab590a9485fc1c
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-22 19:58:33 +0530

    while loop eg

---
+++ JS/file21.js
@@ -0,0 +1,16 @@
+//while loop example
+let num = 10;
+
+let total = 0;
+let i = 0;
+
+while(i<=10){
+    total = total + i;
+    i++;
+}
+console.log(total);
+
+let n = 100;
+let sum = 0;
+sum = (n*(n+1))/2; //this method is fast
+console.log(sum);
