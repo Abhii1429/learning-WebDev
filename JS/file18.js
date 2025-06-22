e155a1c96972b4500a949eb37a063e6a41a95319
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-22 19:40:24 +0530

    if & else-if

---
+++ JS/file18.js
@@ -0,0 +1,34 @@
+//if
+//else if
+let tempInDegree = 45;
+
+// if(tempInDegree < 0){
+//     console.log("extremely cold outside");
+// }else if(tempInDegree < 16){
+//     console.log("it is cold outside");
+// } else if(tempInDegree < 25){
+//     console.log("weather is okay");
+// }else if(tempInDegree < 35){
+//     console.log("let's go for swim");
+// }else if(tempInDegree < 45){
+//     console.log("turn on AC");
+// }else{
+//     console.log("too hot !!");
+// }
+
+
+if(tempInDegree > 40){
+    console.log("too hot !!");
+}else if(tempInDegree > 30){
+    console.log("let's go for swim");
+} else if(tempInDegree > 20){
+    console.log("weather is okay");
+}else if(tempInDegree < 35){
+    console.log("weather is cool");
+}else if(tempInDegree > 10){
+    console.log("it is cold outside");
+}else{
+    console.log("extremely cold");
+}
+
+console.log("Hello ");
