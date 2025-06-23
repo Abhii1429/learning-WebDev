b1cbe017c0b6e927bc448b2c77d8dc4182ecc063
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-23 16:17:21 +0530

    do-while loop

---
+++ JS/file25.js
@@ -0,0 +1,14 @@
+//Do While loop
+
+let i = 10;
+// while(i <= 9){ //while loop
+//     console.log(i);
+//     i++;
+// }
+
+do{ //do while loop works even when initial condition is wrong
+    console.log(i);
+    i++;
+}while(i <= 9);
+
+console.log("value of i is ", i); //output: the value is 11
