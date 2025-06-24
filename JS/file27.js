44a215df997464f44cae1bd1ec7f4726b1674bd5
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-24 12:17:04 +0530

    push/pop unshift/shift

---
+++ JS/file27.js
@@ -0,0 +1,28 @@
+// array push pop
+
+// array shift unshift
+
+let fruits = ["apple", "mango","grapes"];
+console.log(fruits);
+
+//push: adds in the last of the array
+// fruits.push("banana");
+// console.log(fruits); //output: apple, mango,grapes,banana
+
+//pop():removes from the last
+// console.log(fruits.pop());//printing the return value provided by pop() method
+
+// let poppedFruit = fruits.pop();
+// console.log(fruits);
+// console.log("popped fruits is ", poppedFruit);
+
+//unshift: adds in front of the array
+ 
+// fruits.unshift("banana");
+// fruits.unshift("myFruit");
+// console.log(fruits);
+
+//shif():
+let removedFruit = fruits.shift();
+console.log(fruits);
+console.log("removed fruit is", removedFruit);
