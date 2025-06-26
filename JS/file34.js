71928ff0588377ea5ed7b6eb6361cc3e58d815fd
Abhishek <hopesaliveabhi2003@gmail.com>
2025-06-26 11:06:56 +0530

    for in loop in array

---
+++ JS/file34.js
@@ -0,0 +1,15 @@
+//for in loop in array
+const fruits = ["apple","mango","grapes","fruit4", "fruit5"];
+
+const fruits2 =[];
+
+// for(let index in fruits){
+//     fruits2.push(fruits[index].toUpperCase());
+// }
+// console.log(fruits2);
+
+// mostly we use for of loop & traditional for loop in js
+
+for (let i = 0; i <fruits.length; i++){
+    console.log(fruits[i]);
+}
