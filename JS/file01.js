9bb741d959d79876921cfbf94c7be615b7c33c78
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-25 18:51:26 +0530

    printing syntax in js

--- JS/file01.js
+++ JS/file01.js
@@ -1,2 +1,4 @@
 // console.log can print something on console
+//single/double inverted commas or Back ticks(``) are used to represent string data inside console.log
+//
 console.log("Hello World !");
