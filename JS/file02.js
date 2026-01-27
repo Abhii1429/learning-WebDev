09b95625e97aa1b56a9273bf35f724356acf0015
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-27 20:32:29 +0530

    Intro to Variables

--- JS/file02.js
+++ JS/file02.js
@@ -1,8 +1,16 @@
-"use strict";
-//Intro to Variable
+"use strict"; // Enables strict mode  which helps catch common coding errors
+
+//Intro to Variable:
+
 //variable - container to store data/value
+// variables can store some information 
+// we can "use" & "change" that information later in our program
 
-
+//var, let, const - keywords to declare variable
+// variable name is Case sensitive
+// variable name cannot start with number
+// variable name cannot have spaces
+// we use camelCase writing style for variable naming
 
 //declare a variable
 var firstName = "Abhishek";
