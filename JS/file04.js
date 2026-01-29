a1bfc4593771b672b350c8155d6aef1278342b1a
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-29 20:48:28 +0530

    Let keyword

--- JS/file04.js
+++ JS/file04.js
@@ -1,4 +1,10 @@
-//let keyword
+// let keyword:
+
+// it is block scoped variable
+// it cannot be redeclared
+// it can be updated
+// it is hoisted but not initialized
+// it is introduced in ES2015 
 
 // let firstName = "abhishek";
 // console.log(firstName)
@@ -7,7 +13,7 @@
 // var firstName = "mithoo"; this kind of updation cannot be done in Let case
 
 let firstName = "abhishek";
-// let firstName = "mithoo"; //it shows error
+// let firstName = "mithoo"; //it shows error because redeclaration is not allowed in let case  
 firstName = "mithoo";
 
 console.log(firstName);
