6866059103accd91c51416afb409ad3a08249435
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-30 20:24:06 +0530

    String Methods in JavaScript

--- JS/file07.js
+++ JS/file07.js
@@ -1,20 +1,37 @@
-//trim()-->removes the empty spaces
-//toUpperCase()-->makes strings in capital format
-//toLowerCase()-->makes string in lower case format
-//slice-->used to extract a portion of an array or string.
+//String Methods in JavaScript:
+
+//trim()--> removes the empty spaces
+//toUpperCase()--> makes strings in capital format
+//toLowerCase()--> makes string in lower case format
+//slice--> used to extract a portion of an array or string.
  
-// let firstName = "  abhishek  ";
+// let firstName = "  abhishek  "; // string with spaces
 let firstName = "AbhiShek";
 // console.log(firstName);
 console.log(firstName.length);
+
+// trim method:
+
+// it does not change the original string, it returns a new string after removing the spaces.
+// it only removes the spaces from the beginning and the end of the string, not from the middle.
 
 // firstName = firstName.trim(); //"abhishek" 
 // console.log(firstName);
 // console.log(firstName.length);
 
-// firstName = firstName.toUpperCase();
-firstName = firstName.toLowerCase();
+// toUpperCase and toLowerCase methods:
+
+// firstName = firstName.toUpperCase(); // output: ABHISHEK
+
+// .toUpperCase() method does not change the original string, it returns a new string in uppercase.
+
+firstName = firstName.toLowerCase(); // output: abhishek
 console.log(firstName);
+
+// slice method : 
+// it extracts a part of string and returns it as a new string, without modifying the original string.
+// it takes two arguments: start index and end index (end index is optional)
+// if end index is not provided, it extracts till the end of the string.
 
 // let newString = firstName.slice(0,4);//abhi
 // let newString = firstName.slice(0,6);//abhish
