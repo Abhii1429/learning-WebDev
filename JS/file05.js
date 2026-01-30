c25643d0a28d3f6b794d5c268175fecd2b6070c7
Abhishek <hopesaliveabhi2003@gmail.com>
2026-01-30 16:44:41 +0530

    declaring constant variable

--- JS/file05.js
+++ JS/file05.js
@@ -1,4 +1,8 @@
-//declare variable
+//declaring a Constant variable
+
+// const cannot be updated or redeclared because its value is constant
+// it is block scoped variable
+
 const pi = 3.14;
 // pi = 3.15;//we cannnot assign different value to a contant variable.
-console.log(pi);
+console.log(pi*2*2); // const cannot be updated or redeclared but operations can be performed using it.
