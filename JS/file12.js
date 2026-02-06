082dfb41f6e9ea9968153d9efa8a4839221b7788
Abhishek <hopesaliveabhi2003@gmail.com>
2026-02-06 15:17:49 +0530

    booleans & comparison operator

--- JS/file12.js
+++ JS/file12.js
@@ -1,4 +1,7 @@
-//booleans & comparison operator
+// booleans & comparison operator
+
+// comparison operators --> to compare two values
+// > , < , >= , <= , == , === , != , !==
 
 // booleans --> true or false
  
@@ -10,18 +13,28 @@ let num4 = "8";
 console.log(num1 >= num2 );
 console.log(num1 <= num2 );
 console.log(num1 <= num3);
+//----------------------
 
 // == vs ===
+
 // == : only notes values not data types(only in JS)
 
-//=== :checks both value & D.T.
+//=== :checks both, value & D.T.
 
 console.log(num1 == num3);
-console.log(num1 == num4);//only checks value not D.T.
+console.log(num1 == num4);//only checks value NOT D.T.
 
-console.log(num1 === num4);//checks both
+console.log(num1 === num4);//checks both value and D.T.
+//----------------------
 
-// != vs !==
+// != vs !== 
+// != checks Data type(D.T) only, npt value
+// !== chceks D.T & value both
+
+// let num1 = 7;
+// let num2 = "7";
+// console.log(num1 != num2); // output: false
+
 console.log(num1 != num3);//false--> it doesn't check value and DT
 
 console.log(num1 !== num4); //true -->it checks value and DT
