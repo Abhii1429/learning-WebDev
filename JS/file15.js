d0463c324c661d50c8fccced15891d692785362e
Abhishek <hopesaliveabhi2003@gmail.com>
2026-02-06 15:19:30 +0530

    Ternary operator or Conditional operator

--- JS/file15.js
+++ JS/file15.js
@@ -1,15 +1,21 @@
-//ternary operator
+// ternary operator
 
 // let age = 15;
 // let drink;
+
 //  if(age >= 5){
 //     drink = "coffee";
 //  }else{
 //     drink = "milk";
 //  }
 // console.log(drink);
+//-----------------------
 
-//Ternary operator or Conditional operator
+// Ternary operator or Conditional operator
+
+// syntax=> condition ? expression1 : expression2
+// if condition is true, expression1 is executed
+// if condition is false, expression2 is executed
  
 let age = 8;
 let drink = age >= 5 ? "coffee" : "milk";
