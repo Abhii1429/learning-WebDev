52935d9293018485a1799e3302df036b20ef6a67
Abhishek <hopesaliveabhi2003@gmail.com>
2026-02-06 15:18:40 +0530

    if else condition

--- JS/file14.js
+++ JS/file14.js
@@ -6,7 +6,9 @@ let age = 18;
  }else{
     console.log("User can play Temple Run");
  }
+//----------------------
 
+// odd even program
 
  let num = 13;
  if(num%2 === 0){
@@ -14,3 +16,12 @@ let age = 18;
  }else{
     console.log("odd");
  }
+//----------------------
+
+//falsy values:
+
+// false
+// " "
+//null
+// undefined
+// 0
