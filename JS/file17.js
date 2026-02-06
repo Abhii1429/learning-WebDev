6ab1693ff102ac8c77ca2bbba7e9fbf18174babd
Abhishek <hopesaliveabhi2003@gmail.com>
2026-02-06 15:19:58 +0530

    nested if-else

--- JS/file17.js
+++ JS/file17.js
@@ -1,22 +1,25 @@
-//Nested if - else
+// nested if-else:
 
-//winning number --> 19
+// winning number --> 19
+
 //logic
 //19: your guess is right
 // 17 too low
 //28 too high
 
 let winningNumber = 19;
-let userGuess = +prompt("Guess a number");
+let userGuess = +prompt("Guess a number"); // by adding + before prompt, we are converting the string input to number
 
-//prompt() takes input from user in the form of Strings
+// NOTE**: by default, "prompt()" takes input from user in the form of Strings
+
 // console.log(typeof userGuess, userGuess);
 
 if(userGuess === winningNumber){
     console.log("Your guess is right !");
 }else{
     // console.log("Your guess is wrong !");
-    if(userGuess < winningNumber){
+    // code for being more specific
+    if(userGuess < winningNumber){ // nested if else
         console.log("too low!!");
     }else{
         console.log("too high!!");
