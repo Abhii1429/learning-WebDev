c31caf3b6f7e01e32114b366a7956a7aa9e93ffe
Abhishek <hopesaliveabhi2003@gmail.com>
2026-02-07 23:58:10 +0530

    while loop

--- JS/file20.js
+++ JS/file20.js
@@ -1,10 +1,42 @@
 // while loop
 
+// syntax:
+// while(condition){
+//     // code
+//     // updation
+// }
+
+// print numbers from 0 to 9
+// let i = 0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;    
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;    
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;  
+
+// DRY => Do Not Reapeat 
+// A/c to dry coding principle, codes should not be repeated
+
 let i = 0;
 
 while(i <= 9){
     console.log(i);
     i++;
 }
-console.log(`current value of i is ${i}`);
+console.log(`current value of i is ${i}`); // template string used in this
 console.log("hello");
