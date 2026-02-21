f4e87c9e3650aa3a12023c6a179929cf8ae67e1b
Abhishek <hopesaliveabhi2003@gmail.com>
2026-02-21 17:37:18 +0530

    intro to loop

--- JS/file22.js
+++ JS/file22.js
@@ -3,13 +3,13 @@
 for(let i = 0; i <= 9; i++){
      console.log(i)
 }
-console.log(i);// #1 "let" exist only inside loop cannot be accessed outside the loop
+console.log(i);// #1 "let" exist only inside loop "cannot" be accessed outside the loop
 
 
 for(var i = 0; i <= 9; i++){
      console.log(i)
 }
-console.log(i);// #2 "var" can be accessed from outside the loop also
+console.log(i);// imp#2 "var" can be accessed from outside the loop also
 
 // #3 if we declare "let i" outside the loop then we can use this inside the loop and it is also accessible outside the loop as well
 // example:
