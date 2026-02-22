c92c49c341bbf5ef2a433278608fd3b146bb6356
Abhishek <hopesaliveabhi2003@gmail.com>
2026-02-22 13:44:57 +0530

    intro to Arr, ref type, arr indexing

--- JS/file26.js
+++ JS/file26.js
@@ -4,6 +4,7 @@
 // array indexing
 
 // intro:
+
 // Array--> ordered collection of items 
 // Array is reference type thus called object 
 // thus Array is an Object.
@@ -12,16 +13,20 @@
 //# In JavaScript, arrays are objects under the hood, but they are specialized for indexed, ordered data, while objects are designed for key–value mappings. Both are reference types, but they serve different purposes.
 //-------------------------------
 
-//# refrence type: data types that are stored as reference. When we assign an array or object to a variable, we are actually assigning a reference to that data in memory, not the data itself. This means that if we change the array or object through one variable, it will affect all variables that reference the same data.
+//# refrence type:
+//  data types that are stored as reference. When we assign an array or object to a variable, we are actually assigning a reference to that data in memory, not the data itself. This means that if we change the array or object through one variable, it will affect all variables that reference the same data.
+
 //  Array is also a reference type because when we create an array and assign it to a variable, we are actually assigning a reference to that array in memory. When we modify the array through one variable, it will affect all variables that reference the same array.
 // ----------------------------------------------------------------
 
 // how to create array?:
+
 // let arrayName = [item1, item2, item3,...]; // array literal syntax
 // let arrayName = new Array(item1, item2, item3,...); // array constructor syntax
 //---------------------------------------------------------------
 
 // array indexing:
+
 // array indexing starts from 0. Thus, the first element of the array is at index 0, the second element is at index 1, and so on. The last element of the array is at index length-1, where length is the number of elements in the array. We can access any element of the array using its index by using square brackets [].
 // --------------------------------------------------------------
 
