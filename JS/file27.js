7f5674072a7a8ffd12b03e7e692dfd0c383de134
Abhishek <hopesaliveabhi2003@gmail.com>
2026-02-22 13:48:03 +0530

    Arr push, pop, shift, unshift

--- JS/file27.js
+++ JS/file27.js
@@ -8,6 +8,8 @@ console.log(fruits);
 
 // fruits.push("banana");
 // console.log(fruits); // output: apple, mango,grapes,banana
+// # push() method adds one or more elements to the end of an array and returns the new length of the array. It modifies the original array.
+//------------------------------------------------------
 
 //# pop():removes from the last of the array
 
@@ -17,14 +19,18 @@ console.log(fruits);
 // let poppedFruit = fruits.pop();
 // console.log(fruits);
 // console.log("popped fruits is ", poppedFruit);
-
+// # pop() method removes the last element from an array and returns that element. It modifies the original array.
+//------------------------------------------------------
 //# unshift: adds in front of the array
  
 // fruits.unshift("banana");
 // fruits.unshift("myFruit");
 // console.log(fruits);
+// # unshift() method adds one or more elements to the beginning of an array and returns the new length of the array. It modifies the original array.
+//-----------------------------------------------
 
 //shif(): removes from the front of the array
+
 let removedFruit = fruits.shift();
 console.log(fruits);
 console.log("removed fruit is", removedFruit);
