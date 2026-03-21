c45da760e2b6ced60c1667b701304def11dbf928
Abhishek <hopesaliveabhi2003@gmail.com>
2026-03-21 17:39:51 +0530

    cloning of Arr, concatenation of two arr

--- JS/file29.js
+++ JS/file29.js
@@ -4,19 +4,32 @@
 
 
 let array1 = ["item1", "item2"];
-// let array2 = ["item1", "item2"];
+// let array2 = array1;
+// console.log(array1 === array2); // true ; becoz both are stored in same memory location & are reference to the same array in memory.
 
-// let array2 = array1.slice(0);//method 1
+// let array2 = ["item1", "item2"];
+// console.log(array1 === array2); // false ; becoz both are stored in different memory locations & are independent of each other
+
+// Cloning Array:
+
+// method 1: this method is faster than other two
+
+// let array2 = array1.slice(0);
+
 // let array2 = array1.slice(0).concat(["item3", "item4"]);
 
-// let array2 = [].concat(array1);// method 2
+// method 2 :
+
+// let array2 = [].concat(array1);
 //  let array2 = [].concat(array1, ["item3", "item4"]);
 
-//method 3 --> Spread operator
+// #method 3 --> Spread operator  => this method is mostly used in JS.
 
 // let array2 = [...array1,"item3", "item4", "item5"];
+
 let oneMoreArray = ["Ram", "Shyam"];
-let array2 = [...array1, ...oneMoreArray];
+
+// let array2 = [...array1, ...oneMoreArray]; // output : ["item1", "item2", "Ram", "Shyam"]
 
 // array1.push("item3");
 console.log(array1 === array2);
