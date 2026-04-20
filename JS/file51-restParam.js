b90bd8969fe53fb168728f96babdb3f817a8e806
Abhishek Pandey <hopesaliveabhi2003@gmail.com>
2026-04-20 11:18:57 +0530

    rest parameter

--- JS/file51-restParam.js
+++ JS/file51-restParam.js
@@ -9,10 +9,26 @@
 // }
 // myFunc(3, 4, 5);
 
-function myFunc(a, b, ...c){
-    console.log(`a is ${a}`);
-    console.log(`b is ${b}`);
-    console.log(`c is `, c);
+// function myFunc(a, b, ...c){
+//     console.log(`a is ${a}`);
+//     console.log(`b is ${b}`);
+//     console.log(`c is `, c);
+// }
+// myFunc(3, 4, 5, 6, 7, 8, 9);
+
+function addAll(...numbers){
+    // console.log(numbers);
+    // console.log(typeof numbers); // object, because arrays are objects in JavaScript
+
+    let total = 0;
+    for(let number of numbers){
+        total = total + number;
+    }
+    return total;
 }
-myFunc(3, 4, 5, 6, 7, 8, 9);
+// addAll(1,2,3,4,5,6,7,8,9);
+// const ans = addAll(1,2,3,4,5,6,7,8,9); // 45
+const ans = addAll(4,5,4); // 13
+
+console.log(ans);
 
