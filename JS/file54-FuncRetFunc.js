8966d2f360809cf02ec63287755bd2f5ab51c6b3
Abhishek Pandey <hopesaliveabhi2003@gmail.com>
2026-04-20 11:20:14 +0530

    function returning function

---
+++ JS/file54-FuncRetFunc.js
@@ -0,0 +1,11 @@
+// Function returning function:;
+
+function myFunc(){
+    // return a;
+    // return [1, 2, 3]; // return an array
+    return{name : "Abhishek", age : 23}; // return an object
+}
+
+const ans = myFunc(); // ans will store the return value of myFunc()
+console.log(ans);
+
